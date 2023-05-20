c17362d59f5fba37d5bd03a0c2e7bbaa42b71bb2
yeasin550 <yeasin51570@gmail.com>
2023-05-20 08:40:55 +0600

    create private route in mytoys and add a toys page

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -9,7 +9,7 @@ import { Navigate, useLocation } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
    const { user, loading } = useContext(AuthContext);
    const location = useLocation();
-  //  console.log(location);
+  
    if (loading) {
      return <progress className="progress w-56 text-center"></progress>;
    }
